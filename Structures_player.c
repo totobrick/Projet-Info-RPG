ca170f5113dd90aac97257e275f69db2a642e21d
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-17 19:35:58 +0200

    Update Structures_player.c

--- Structures_player.c
+++ Structures_player.c
@@ -2,7 +2,7 @@
 
 typedef struct{
   char nom[100]; //ATTENTION depasement tableau
-  int classe;
+  int classe; //Magicien, guerrier, ranger, voleur
   int arme; //1,2,3,4
   int replique; //0,1
 }Joueur;
