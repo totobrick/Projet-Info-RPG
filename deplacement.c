c095bb90751c25e552135a61d490e126552f2d63
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-18 10:50:24 +0200

    Update deplacement.c

--- deplacement.c
+++ deplacement.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
-int deplacement_joueur;
-int deplacement_case;
+int deplacement_joueur = 0;
+int deplacement_case = 0;
 
 while (deplacement_joueur != deplacement_case){
-
+    
+  if deplacement_jouer == 8; // déplacement vers le Haut
+  if deplacement_jouer == 2; // déplacement vers le Bas
+  if deplacement_jouer == 4; // déplacement vers la Gauche
+  if deplacement_jouer == 6; // déplacement vers la Droite
+  
 }
