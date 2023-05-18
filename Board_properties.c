b0f461fc9ac876b31dde26dc0be047db030a5e23
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 11:01:46 +0200

    Update Board_properties.c

--- Board_properties.c
+++ Board_properties.c
@@ -2,13 +2,22 @@
 #define SIZE 8		//tableau 6x6
 
 typedef struct{
-
+	int mur;	//1=oui 0=non
 } space;		//space = 1 case du jeu
 
 void board (space tab, int size){
-	for (int i=0 ; i<=size ; i++){
-		for (int j=0 ; j<=size ; j++){
-			
+	for (int i=1 ; i<(size-1) ; i++){
+		for (int j=0 ; j<size ; j++){
+			tab[i][j]
 		}
+	}
+}
+
+void show_board (space tab, int size){
+	for (int i=0 ; i<size ; i++){
+		for (int j=0 ; j<size ; j++){
+			printf("	", tab[i][j]);		//A COMPLETER
+		}
+		printf("\n", tab[i][j]);			//A COMPLETER
 	}
 }
