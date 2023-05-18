3704510ab196bbdbb3bc2b8eac2ac46c46471392
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 10:28:48 +0200

    Update evenement.c

--- evenement.c
+++ evenement.c
@@ -1,8 +1,8 @@
     typedef struct{
-    int neutral;  // 0=non et 1=oui 
-    int shop;   // 0=non et 1=oui 
-    int malus;  // 0=non et 1=oui 
-    int bonus; // 0=non et 1=oui 
+    int neutral;    // 0=non et 1=oui 
+    int shop;       // 0=non et 1=oui 
+    int malus;      // 0=non et 1=oui 
+    int bonus;      // 0=non et 1=oui 
 } event;
 
 if (malus==1 && neutral==0 && shop==0 && bonus==0)
