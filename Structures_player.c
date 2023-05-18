f97fa52a3d8a7a49a42ecf23bdcf05da43ca5851
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 21:12:11 +0200

    Update Structures_player.c

--- Structures_player.c
+++ Structures_player.c
@@ -2,7 +2,22 @@
 
 typedef struct{
   char nom[100]; //ATTENTION depasement tableau
-  int classe; //Magicien, guerrier, ranger, voleur
-  int arme; //1,2,3,4
-  int replique; //0,1
+  int class; //Magicien, guerrier, ranger, voleur
+  int weapon; //1,2,3,4
+  int relic; //0,1
 }Joueur;
+
+
+void vide_buffer(){
+  while (getchar()!='/n'){
+  }
+}
+int main() {
+  int class;
+  int verif;
+  do{
+    printf("Quelle est votre classe?");
+    verif=scanf("%d",&class);
+    vide_buffer();
+  }while(class !=1 || class !=2 || class !=3 || class !=4 || verif !=1);
+
