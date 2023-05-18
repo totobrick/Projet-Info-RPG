e4171fbc2aa4a5bbad7866d3284eb6c5695b4a47
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 21:23:24 +0200

    Update Structures_player.c

--- Structures_player.c
+++ Structures_player.c
@@ -16,12 +16,15 @@ void vide_buffer() {
     }
 }
 
-int main() {
-    int classe;
+int main() {  
+    class = 0
     int verif = 0;
+   
 
     do {
         printf("Quelle est votre classe ? ");
         verif = scanf("%d", &classe);
         vide_buffer();
     } while (classe != 1 && classe != 2 && classe != 3 && classe != 4 || verif != 1);
+  
+  
