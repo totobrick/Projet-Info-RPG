2849a56a1480caa92d52e27feb9f7636969cc7f0
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 13:14:21 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -7,7 +7,13 @@
 #define SIZE 8		//tableau 6x6
 
 int main(){
-    board = malloc(sizeof(card)*8*8);
+    card* board;                                //allocation espace mémoire
+    board = malloc(sizeof(card)*8*8);           // board est l'adresse de la 1ere case du tableau
+    if (board == NULL){
+        printf("Echec de la création du plateau de jeu !\n");
+        exit(1);
+    }
     
+    free(board);                                //restitution espace mémoire
     return 0;
 }
