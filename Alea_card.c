e9ab593a3e405f0bdb5f32506183c9b0b5dbc405
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 21:26:53 +0200

    Update Alea_card.c

--- Alea_card.c
+++ Alea_card.c
@@ -1,4 +1,19 @@
 void generate_board (card* tab, int size, int nbre_card){       // il y a 36-4=32 cartes à coder, 4 seront vierges (pour les perso)
+    /*Pour générer la 1ere carte :
+        de 0 à 15 on aura un monstre
+            -> 0 à 3 : Basilic
+            -> 4 à 7 : Zombie
+            -> 8 à 11 : Troll
+            -> 12 à 15 : Harpi
+    
+    
+    
+    */
+    int basilic = 4;
+    int zombie = 4;
+    int troll = 4;
+    int harpie = 4;
+    int 
     int alea = rand() % nbre_card;
 
 }
