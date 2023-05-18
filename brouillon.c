fa4706ea35759f218654a032939b2665fe032f39
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 14:46:55 +0200

    Update brouillon.c

--- brouillon.c
+++ brouillon.c
@@ -74,3 +74,19 @@ int main() {
 }
 */
 
+//rick roll bis
+/*
+#include <unistd.h>
+
+int main() {
+    const char* rickRollURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";  // Lien vers la vidéo de Rick Astley
+
+    const char* browser = "xdg-open";  // Commande pour ouvrir l'URL avec le navigateur par défaut sur Linux
+
+    // Lancement du navigateur avec l'URL du Rick Roll
+    execlp(browser, browser, rickRollURL, NULL);
+
+    return 0;
+}
+
+*/
