4ceddfc87bd889563337c87a3edecce35e45cc2c
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 20:41:29 +0200

    Update Graphisme.c

--- Graphisme.c
+++ Graphisme.c
@@ -22,28 +22,12 @@ Le mage :
 "m" (code ASCII 109)
 "Ψ" (code ASCII 936)
 "Ϟ" (code ASCII 990)
-L'elfe :
 
-"E" (code ASCII 69)
-"e" (code ASCII 101)
-"Ǝ" (code ASCII 398)
-"ɘ" (code ASCII 600)
-Le nain :
-
-"N" (code ASCII 78)
-"n" (code ASCII 110)
-"ñ" (code ASCII 241)
-"Ӣ" (code ASCII 1246)
 Le ranger :
 
 "R" (code ASCII 82)
 "r" (code ASCII 114)
 "Ř" (code ASCII 344)
 "Ɍ" (code ASCII 428)
-Le guerrier :
 
-"W" (code ASCII 87)
-"w" (code ASCII 119)
-"Щ" (code ASCII 1065)
-"Ѡ" (code ASCII 1136)
   
