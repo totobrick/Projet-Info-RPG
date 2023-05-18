8b539e6fdb42d021954d47bfca69086c76e8f067
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 20:40:58 +0200

    Update Graphisme.c

--- Graphisme.c
+++ Graphisme.c
@@ -1,7 +1,16 @@
 //logo et couleur
 
+"T" (code ASCII 84) : Ce caractère peut représenter un voleur de manière stylisée, avec ses bras étendus pour symboliser la discrétion et la rapidité.
 
-Le chevalier :
+"%" (code ASCII 37) : Ce caractère peut représenter un voleur avec son masque ou son bandeau sur le visage, symbolisant l'anonymat et la clandestinité.
+
+"$" (code ASCII 36) : Ce caractère peut être utilisé pour représenter un voleur en référence à l'argent, un élément souvent associé aux activités des voleurs.
+
+"@" (code ASCII 64) : Ce caractère peut représenter un voleur dans une position furtive ou accroupie, prêt à agir rapidement.
+
+"Ϡ" (code ASCII 1008) : Ce caractère ressemble à une silhouette penchée ou furtive, ce qui peut évoquer l'image d'un voleur.
+
+  Le chevalier :
 
 "K" (code ASCII 75)
 "k" (code ASCII 107)
