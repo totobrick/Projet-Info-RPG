edbeeb790c332dfba74b25867e0bbb2126261347
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-18 11:07:15 +0200

    Update deplacement.c

--- deplacement.c
+++ deplacement.c
@@ -3,11 +3,23 @@
 int deplacement_joueur = 0;
 int deplacement_case = 0;
 
+
+printf("Maintenant choisissez une direction :"\n)
+printf(\t,\t,"Haut")
+
+
+
+
+
+
+
 while (deplacement_joueur != deplacement_case){
-    
-  if deplacement_jouer == 8; // déplacement vers le Haut
-  if deplacement_jouer == 2; // déplacement vers le Bas
-  if deplacement_jouer == 4; // déplacement vers la Gauche
-  if deplacement_jouer == 6; // déplacement vers la Droite
+     break
+     printf("Maintenant choisissez une direction :")
+     scanf("%d",deplacement_joueur);
+     if deplacement_joueur == 8; // déplacement vers le Haut
+     if deplacement_joueur == 2; // déplacement vers le Bas
+     if deplacement_joueur == 4; // déplacement vers la Gauche
+     if deplacement_joueur == 6; // déplacement vers la Droite
   
 }
