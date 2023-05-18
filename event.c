724f9592468932c42f3c858b82f7e1e4d4260d74
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 10:38:51 +0200

    Update event.c

--- event.c
+++ event.c
@@ -5,7 +5,15 @@
     int bonus;      // 0=non et 1=oui 
 } event;
 
-if (malus==1 && neutral==0 && shop==0 && bonus==0)
+
+
+if (malus==1 && neutral==0 && shop==0 && bonus==0){
+    
+    
+    printf("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. 
+}
 if (malus==0 && neutral==1 && shop==0 && bonus==0)
 if (malus==0 && neutral==0 && shop==1 && bonus==0)
 if (malus==0 && neutral==0 && shop==0 && bonus==1)
+    
+    
