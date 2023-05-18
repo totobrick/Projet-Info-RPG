9e6a003a733df32609705ca4b5a26b5632c64229
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 15:52:56 +0200

    Update and rename event.c to Event.c

--- Event.c
+++ Event.c
@@ -9,8 +9,6 @@
 
 if (malus==1 && neutral==0 && shop==0 && bonus==0){
     
-    srand (time(NULL));
-    
     printf ("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. À cause de cette action, vous vous retrouvez sous une marée draconique, et mourrez, malgré tous avec l'honneur d'un grand guerrier!");
   
     printf ("Vous croisez une espèce de grosse tortue avec plein de pics sur sa carapace, et qui crache du feu. Il vous regarde, et vous demande si vous n'avez pas vu une princesse blonde en robe rose. Vous lui dite que non, et par colère, il vous vole 10 petites étoiles! (si vous possédiez votre relique, elle est retourné à son point d'emplacement)");
@@ -18,11 +16,7 @@ if (malus==1 && neutral==0 && shop==0 && bonus==0){
  //  printf ("Vous repérez au loin un vieux magicien sénil. Vous tentez de vous approchez, pour savoir  si tous va bien. Lorsque vous êtes assez proche pour voir son visage, il vous regarde droits dans les yeux et vous hurle: VOUS NE PASSEREZ PAS! (la case évenemnt devient définitivement une case mur)");
 }
 if (malus==0 && neutral==1 && shop==0 && bonus==0)
-    
-       srand (time(NULL));
-
-
-
+   
     printf ("Un spectacle d'horreur se déroule devant vous! Un pauvre poisson clown, hors de l'eau semble pleurer, tous en ne pouvant pas respirer. Il semble répeter sans cesse ces mêmes mots: Vous n'avez pas vu mon fils? Vous n'avez pas vu mon fils? Vous n'avez pas vu mon...  Le manque d'oxygen a eu raison de lui. Vous avez désormais un nouvelle objectif. après être sorti victorieux de ce labyrinthe infernal, vous vous metterez à la recherche de ce fameux fils.");
 
     printf ("Vous croisez un enorme tuyaux fait de métal . Intrigué, vous vous approchez et entendez une étrange voix dans votre tête: Il faut suivre le conduit. Ne sacchant ce qu'est ce fameux «conduit», vous continuez votre route.");
@@ -36,15 +30,12 @@ if (malus==0 && neutral==1 && shop==0 && bonus==0)
            
     printf("Vous trouvez un parchemin ancien! Quel secret cache t-il? Vous l'ouvrez et contemplez. Il est écrit «Seul Link peut vaincre Ganon.». De part le faite que vous ne vous nommez pas Link ni Ganon, vous jeté ce torchon inutile.")
 
+           
 if (malus==0 && neutral==0 && shop==1 && bonus==0)
-    
-    srand (time(NULL));
            
     
     
     
-if (malus==0 && neutral==0 && shop==0 && bonus==1)
-    
-    srand (time(NULL));    
+if (malus==0 && neutral==0 && shop==0 && bonus==1)  
            
            printf("En vous balladant, vous tomber sur une étrange famille de 
