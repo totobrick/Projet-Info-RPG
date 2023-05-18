4a105cc0771ca954cbbff6138aa77b95951565d0
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 12:49:45 +0200

    Update Board_properties.c

--- Board_properties.c
+++ Board_properties.c
@@ -37,6 +37,7 @@ typedef struct{
 
 void board (card tab, int size){
     init_wall(tab, size);
+    init_card(tab,size);
 }
 
 /*_________________________________________________________________________________________________*/
