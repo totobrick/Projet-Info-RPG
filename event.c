7f2bf92d92dfc876093ea4bf8a1f6aebcbe43b1d
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 10:52:56 +0200

    Update event.c

--- event.c
+++ event.c
@@ -9,8 +9,13 @@
 
 if (malus==1 && neutral==0 && shop==0 && bonus==0){
     
+    srand (time(NULL));
     
-    printf("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. 
+    printf ("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. À cause de cette action, vous vous retrouvez sous une marée draconique, et mourrez, malgré tous avec l'honneur d'un grand guerrier!");
+  
+    printf ("Vous croisez une espèce de grosse tortue avec plein de pics sur sa carapace, et qui crache du feu. Il vous regarde, et vous demande si vous n'avez pas vu une princesse blonde en robe rose. Vous lui dite que non, et par colère, il vous vole 10 petites étoiles! (si vous possédiez votre relique, elle est retourné à son point d'emplacement)");
+    
+   printf ("Vous repérez au loin un vieux magicien sénil. Vous tentez de vous approchez, pour savoir  
 }
 if (malus==0 && neutral==1 && shop==0 && bonus==0)
 if (malus==0 && neutral==0 && shop==1 && bonus==0)
