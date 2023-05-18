f295a08cde4365fdc8d37397f21ff67f54a4c266
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 16:36:54 +0200

    Update Event.c

--- Event.c
+++ Event.c
@@ -9,8 +9,6 @@ typedef struct{
 
 if (malus==1 && neutral==0 && shop==0 && bonus==0){
     
-    srand (time(NULL));
-    
     printf ("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. À cause de cette action, vous vous retrouvez sous une marée draconique, et mourrez, malgré tous avec l'honneur d'un grand guerrier!\n");
   
     printf ("Vous croisez une espèce de grosse tortue avec plein de pics sur sa carapace, et qui crache du feu. Elle vous regarde, et vous demande si vous n'avez pas vu une princesse blonde en robe rose. Vous lui dites que non, et par colère, il vous vole 10 petites étoiles! (si vous possédiez votre relique, elle est retournée à son point d'emplacement)\n");
@@ -18,10 +16,8 @@ if (malus==1 && neutral==0 && shop==0 && bonus==0){
  //  printf ("Vous repérez au loin un vieux magicien sénil. Vous tentez de vous approchez, pour savoir  si tous va bien. Lorsque vous êtes assez proche pour voir son visage, il vous regarde droits dans les yeux et vous hurle: VOUS NE PASSEREZ PAS! (la case évenemnt devient définitivement une case mur)\n");
 }
 else if (malus==0 && neutral==1 && shop==0 && bonus==0)
-    
-    srand (time(NULL));
-
-    printf ("Un spectacle d'horreur se déroule devant vous ! Un pauvre poisson-clown hors de l'eau semble pleurer tout en ne pouvant pas respirer.\nIl semble répéter sans cesse ces mêmes mots :\n                    « Vous n'avez pas vu mon fils ? Vous n'avez pas vu mon fils ? Vous n'avez pas vu mon... » \nLe manque d'oxygen a eu raison de lui. Vous avez désormais un nouvel objectif : après être sorti victorieux de ce labyrinthe infernal, vous vous mettrez à la recherche de ce fameux fils.\n");
+   
+    printf ("Un spectacle d'horreur se déroule devant vous ! Un pauvre poisson-clown hors de l'eau semble pleurer tout en ne pouvant pas respirer.\n Il semble répéter sans cesse ces mêmes mots :\n   « Vous n'avez pas vu mon fils ?/n Vous n'avez pas vu mon fils ?/n Vous n'avez pas vu mon... » \nLe manque d'oxygen a eu raison de lui. Vous avez désormais un nouvel objectif : après être sorti victorieux de ce labyrinthe infernal, vous vous mettrez à la recherche de ce fameux « fils ».\n");
 
     printf ("Vous croisez un énorme tuyau fait de métal. Intrigué, vous vous approchez et entendez une étrange voix dans votre tête : Il faut suivre le conduit. Ne sachant ce qu'est ce fameux « conduit », vous continuez votre route.\n");
             
@@ -31,27 +27,25 @@ else if (malus==0 && neutral==1 && shop==0 && bonus==0)
            //essayer les code qui fonctione le mieux pour la musique
            
     printf("Vous voyez une sorte de scientifique au loin. En essayant de vous approcher, vous voyez une sorte d'écran apparaître avec marqué dessus : « Fun value invalide ». \nVoyant que vous ne pouvez bas continuer, vous décidez d'abandonner et de continuer votre périple.\n");
-           
+                   
+    printf("En vous baladant, vous tombez sur une étrange famille de deux squelettes. L'un petit, portant des habits rouge et blanc. Son frère, plus petit, porte un sweet à capuche bleuet et un pantalon noir. Pour éviter tout combat inutile, vous attendez un peu que le temps passe.");
+
     printf("Vous trouvez un parchemin ancien ! Quel secret cache t-il ? \nVous l'ouvrez et le contemplez. Il est écrit : « Seul Link peut vaincre Ganon. ». Du fait que vous ne vous nommez ni Link, ni Ganon, vous jetez ce torchon inutile.\n");
 
 else if (malus==0 && neutral==0 && shop==1 && bonus==0){
     
-    srand (time(NULL));
+    
 }
            
     
     
     
 else if (malus==0 && neutral==0 && shop==0 && bonus==1){
-    
-    srand (time(NULL));    
            
-           printf("En vous baladant, vous tombez sur une étrange famille de deux squelettes. L'un petit, portant des habits rouge et blanc. Son frère, plus petit, porte un sweet à capuche bleuet et un pantalon noir. Pour éviter tout combat inutile, vous attendez un peu que le temps passe.");
-    
+           printf("Vous rencontrez au détour de ce labyrinthe un vieil homme. Il prétend être le directeur d'un lycée spécialisé en magie très connu: le lycée magique George Pompidou. Il exprime même l'envie de vous faire entrer dans son lycée, dans la classe de « Nintendor ». Vous acceptez, et par conséquent, il se mit à hurlé: «100.000 point pour Nintendor! ».")
                   }
 
                   else(){
                   printf ("Problème ! L'évenement n'a pas pu être lancé !);
-                  
                   }
-}
+ }
