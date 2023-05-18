a8c321403464200003bc0cc9d0aa2309fa2d58b4
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 10:27:44 +0200

    Update evenement.c

--- evenement.c
+++ evenement.c
@@ -1,1 +1,8 @@
-//beaucoup, BEAUCOUP BEAUCOUP de printf
+    typedef struct{
+    int neutral;  // 0=non et 1=oui 
+    int shop;   // 0=non et 1=oui 
+    int malus;  // 0=non et 1=oui 
+    int bonus; // 0=non et 1=oui 
+} event;
+
+if (malus==1 && neutral==0 && shop==0 && bonus==0)
