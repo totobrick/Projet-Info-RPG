cbc3b21fe2ec59ae111102e1a035949185014239
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-18 21:36:25 +0200

    Update Brouillon-combat.c

--- Brouillon-combat.c
+++ Brouillon-combat.c
@@ -3,6 +3,12 @@
 enum Armes { TORCHE = 1, BOUCLIER = 2, HACHE = 3, ARC = 4 };
 enum Ennemis { BASILIC = 1, ZOMBIE = 2, TROLL = 3, HARPIE = 4 };
 
+void vide_buffer() {
+    int c;
+    while ((c = getchar()) != '\n') {
+    }
+}
+
 void combat(int arme, int ennemi) {
     switch (ennemi) {
         case BASILIC:
@@ -41,25 +47,35 @@ void combat(int arme, int ennemi) {
 
 int main() {
     int arme;
-    int ennemi;
+    int ennemi; 
+    int verif = 0;
+  
 
     printf("Choisissez votre arme : \n");
     printf("1. Torche\n");
     printf("2. Bouclier\n");
     printf("3. Hache\n");
     printf("4. Arc\n");
-    printf("Votre choix : ");
-    scanf("%d", &arme);
-
+      do {
+        printf("Votre choix: ");
+        verif = scanf("%d", &arme);
+        vide_buffer();
+    } while (arme != 1 && arme != 2 && arme != 3 && arme != 4 || arme != 1);
+verif=0;
     printf("Choisissez votre ennemi : \n");
     printf("1. Basilic\n");
     printf("2. Zombie\n");
     printf("3. Troll\n");
     printf("4. Harpie\n");
-    printf("Votre choix : ");
-    scanf("%d", &ennemi);
+       do {
+        printf("Votre choix:  ");
+        verif = scanf("%d", &ennemi);
+        vide_buffer();
+    } while (ennemi != 1 && ennemi != 2 && ennemi != 3 && ennemi != 4 || ennemi != 1);
 
     combat(arme, ennemi);
 
     return 0;
 }
+
+ 
