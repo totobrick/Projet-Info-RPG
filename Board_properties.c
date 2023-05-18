73646a42fa0d9d3a12be2654ab78028ba8dfd0a2
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 10:45:50 +0200

    Update Board_properties.c

--- Board_properties.c
+++ Board_properties.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#define SIZE 6		//tableau 6x6
+#define SIZE 8		//tableau 6x6
 
-void board (case tab, int size){
-	
+typedef struct{
+
+} space;		//space = 1 case du jeu
+
+void board (space tab, int size){
+	for (int i=0 ; i<=size ; i++){
+		for (int j=0 ; j<=size ; j++){
+			
+		}
+	}
 }
