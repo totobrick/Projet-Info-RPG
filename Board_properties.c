e66c6326f3c41ce73a2ddcf52c1f4554042f8e61
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-18 11:37:41 +0200

    Update Board_properties.c

--- Board_properties.c
+++ Board_properties.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 #define SIZE 8		//tableau 6x6
 
+#include<stdio.h>
+
+typedef struct{
+    int zombie;  // 1, < Torche
+    int troll;   // 2, < Hache
+    int harpy;  // 3, < Arc
+    int basilisk; // 4, < Bouclier				basilisk = basilic
+} monster;
+
+typedef struct{
+    int stick;      	//stick = baton
+    int sword;      	//sword = epee
+    int grimoire;
+    int dagger;			//dagger = dague
+} relic;
+
 typedef struct{
 	int mur;	//1=oui 0=non
-} space;		//space = 1 case du jeu
+	int hidden;	//0=cache 1= case visible
+    monster m;
+    int treasure;
+    int totem;
+    relic r;
+    int portal;			//portal = portail
+    int event;
+    //evenement????
+} space;
+
 
 void board (space tab, int size){
+	for (int j=0; j<size ; j++){
+		tab[0][j].mur=1;
+	}
 	for (int i=1 ; i<(size-1) ; i++){
-		for (int j=0 ; j<size ; j++){
-			tab[i][j]
+		tab[i][0].mur=1;
+		for (int j=1 ; j<(size-1) ; j++){
+			tab[i][j].mur=0;
 		}
+		tab[i][7].mur=1;
+	}
+	for (int j=0; j<size ; j++){
+		tab[size-1][j].mur=1;
 	}
 }
 
@@ -20,4 +53,12 @@ void show_board (space tab, int size){
 		}
 		printf("\n", tab[i][j]);			//A COMPLETER
 	}
+}
+
+void return_box(space tab, int size){
+	for (int i=1 ; i<(size-1) ; i++){
+		tab[i][0].mur=1;
+		for (int j=1 ; j<(size-1) ; j++){
+			tab[i][j].mur=0;
+		}
 }
