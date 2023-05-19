e951e48c44e263da993dbbee81a77cbe407ec08a
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-19 11:16:59 +0200

    Update Event.c

--- Event.c
+++ Event.c
@@ -8,29 +8,29 @@ int m1, m2, m3, m4, n1, n2, n3, n4, n5, n6, n7 = 1;
 
 if (malus==1 && neutral==0){
     do{
-   int randmalus = rand() %101;
+   int randmalus = rand() %4;
     
-    if (randmalus < 26 && m1==1){
+    if (randmalus == 0 && m1==1){
     
     printf(" ");
            //essayer les code qui fonctione le mieux pour la musique
         m1 = 0;
         return;
     }
-        if (randmalus > 25 && randmalus < 51 && m2==1){
+        if (randmalus == 1 && m2==1){
             
     printf ("Une harpie sauvage apparaît! « It's over Anakin, I have the highground! », dit-elle.\n")
         //change la case en monstre harpy et lance le combat. la case devient définitivement une harpy.
         m2=0;
             return;
             }
-        if (randmalus > 50 && randmalus < 76  && m3==1){
+        if (randmalus == 2 && m3==1){
             
     printf ("Vous voyez au loin un homme qui ne bouge pas. Vous vous approchez en pensant qu'il pourrait avoir besoin d'aide. Tous à coup, vous le voyez courrir en hurlant: LEEROY JENKINS!!!. À cause de cette action, vous vous retrouvez sous une marée draconique, et mourrez, malgré tous avec l'honneur d'un grand guerrier!\n");
      m3=0;
          return;
         }
-        if (randmalus > 75 && m4==1){
+        if (randmalus == 3 && m4==1){
         
     printf ("Vous croisez une espèce de grosse tortue avec plein de pics sur sa carapace, et qui crache du feu. Elle vous regarde, et vous demande si vous n'avez pas vu une princesse blonde en robe rose. Vous lui dites que non, et par colère, il vous vole 10 petites étoiles! (si vous possédiez votre relique, elle est retournée à son point d'emplacement)\n");
             m4=0;
@@ -39,7 +39,7 @@ if (malus==1 && neutral==0){
     }   while(m1==1 && m2==1 && m3==1 && m4==1)
 }
 else if (malus==0 && neutral==1){
-   
+   do{
     int randneutre = rand() %7;
     
     if (randneutre==0 && n1==1){
@@ -89,6 +89,7 @@ else if (malus==0 && neutral==1){
             n7=0
         return;
         }
+       while(n1==1 && n2==1 && n3==1 && n4==1 && n5==1 && n6==1 && n7==1)
 }
                   else(){
                   printf ("Problème ! L'évenement n'a pas pu être lancé !\n");
