f301d74f7d7c34b988b7a168a60042708c1bec0b
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-19 12:13:09 +0200

    Update Alea_card.c

--- Alea_card.c
+++ Alea_card.c
@@ -1,16 +1,103 @@
-#include <string.h>
-
-void generate_board (card* tab, int size){
+void generate_board (card tab[][], int size){
     /*Pour générer la 1ere carte :
         de 1 à 16 on aura un monstre
             -> 1 à 4 : Basilic
             -> 5 à 8 : Zombie
             -> 9 à 12 : Troll
             -> 13 à 16 : Harpie
-    
-    
-    
+   
     */
+    char cards[] = { 'b' , 'b' , 'b' , 'b' , 'z' , 'z' , 'z' , 'z' , 't' , 't' , 't' , 't' , 'h' , 'h' , 'h' , 'h' , 'E' , 'B' , 'G' , 'D' , 'C' , 'C' , 'T' , 'T' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' };
+    int remaining_card = 32;                               // remaining_card = cartes_restantes
+    /*
+    cards[] est le tableau de toutes nos cartes à placer aléatoirement.
+    Voici le détail de ces cartes :
+        -> Les 16 monstres :
+            'b' = basilic   (x4)
+            'z' = zombie    (x4)
+            't' = troll     (x4)
+            'h' = harpie    (x4)
+        -> Les 4 armes antiques (reliques) :
+            'E' = épée      (x1)
+            'B' = bâton     (x1)
+            'G' = grimoire  (x1)
+            'D' = dague     (x1)
+        -> Les 2 coffres au trésor :
+            'C' = coffre    (x2)
+        -> Les 2 totems :
+            'T' = totem     (x2)
+        -> Les 8 événements :
+            'e' = événement (x8)
+    */
+    
+    //le tableau tab est déjà initialisé dans une fonction précédente
+    for (int i=1 ; i<(size-1) ; i++){
+        for (int j=1 ; j<(size-1) ; j++){
+            int alea= rand() % remaining_card;
+            //Les 4 monstres
+            if(cards[alea] == 'b'){
+                tab[i][j].m.basilisk = 1;
+            }
+            else if(cards[alea] == 'z'){
+                tab[i][j].m.zombie = 1;
+            }
+            else if(cards[alea] == 't'){
+                tab[i][j].m.troll = 1;
+            }
+            else if(cards[alea] == 'h'){
+                tab[i][j].m.harpy = 1;
+            }
+            //Les 4 armes antiques
+            else if(cards[alea] == 'E'){
+                tab[i][j].r.sword = 1;
+            }
+            else if(cards[alea] == 'B'){
+                tab[i][j].r.stick = 1;
+            }
+            else if(cards[alea] == 'G'){
+                tab[i][j].r.grimoire = 1;
+            }
+            else if(cards[alea] == 'D'){
+                tab[i][j].r.dagger = 1;
+            }
+            // coffre et totem
+            else if(cards[alea] == 'C'){
+                tab[i][j].treasure = 1;
+            }
+            else if(cards[alea] == 'T'){
+                tab[i][j].totem = 1;
+            }
+            // événement
+            else if(cards[alea] == 'e'){
+                tab[i][j].event = 1;
+            }
+            else {
+                printf(" OUPS ! Le jeu n'a pas pu être créé. La carte générée aléatoirement n'est pas dans le paquet.\n");
+                exit(1);
+            }
+            
+            cards[alea] = cards[remaining_card-1];
+            remaining_card = remaining_card -1;
+        }
+        
+    }
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ /*
     int basilisk = 4, zombie = 4, troll = 4, harpy = 4;     //les 16 monstres
     int sword = 1, stick = 1, grimoire = 1, dagger = 1;     //les 4 armes antiques
     int treasure = 2;                                       //les 2 trésors
@@ -35,26 +122,7 @@ void generate_board (card* tab, int size){
             (*(tab + i*size + j))
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-    
-    
+*/
     
     
 /*
