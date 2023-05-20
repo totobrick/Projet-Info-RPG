3288aa20b94bb4d13ea3e3f6cb692bc3870a8ccf
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-20 15:23:07 +0200

    Update Alea_card.c

--- Alea_card.c
+++ Alea_card.c
@@ -4,28 +4,30 @@
 
 void generate_board (card* tab, int size);
 
-void generate_board (card* tab, int size){       //ATTENTION METTRE EN POINTEUR
-    char cards[] = { 'b' , 'b' , 'b' , 'b' , 'z' , 'z' , 'z' , 'z' , 't' , 't' , 't' , 't' , 'h' , 'h' , 'h' , 'h' , 'E' , 'B' , 'G' , 'D' , 'C' , 'C' , 'T' , 'T' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' };
+void generate_board (card* tab, int size){
+    char cards[] = { 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'e' , 'b' , 'b' , 'b' , 'b' , 'z' , 'z' , 'z' , 'z' , 't' , 't' , 't' , 't' , 'h' , 'h' , 'h' , 'h' , 'C' , 'C' , 'T' , 'T' , 'E' , 'B' , 'G' , 'D', 'P' };
     int remaining_card = 32;                               // remaining_card = cartes_restantes
     /*
-    cards[] est le tableau de toutes nos cartes à placer aléatoirement.
+    cards[] est le tableau de toutes nos 32 cartes à placer aléatoirement sur un plateau de 36.
     Voici le détail de ces cartes :
+        -> Les 7 événements :
+            'e' = événement (x7)
         -> Les 16 monstres :
             'b' = basilic   (x4)
             'z' = zombie    (x4)
             't' = troll     (x4)
             'h' = harpie    (x4)
+        -> Les 2 coffres au trésor :
+            'C' = coffre    (x2)
+        -> Les 2 totems :
+            'T' = totem     (x2)
         -> Les 4 armes antiques (reliques) :
             'E' = épée      (x1)
             'B' = bâton     (x1)
             'G' = grimoire  (x1)
             'D' = dague     (x1)
-        -> Les 2 coffres au trésor :
-            'C' = coffre    (x2)
-        -> Les 2 totems :
-            'T' = totem     (x2)
-        -> Les 8 événements :
-            'e' = événement (x8)
+        -> 1 portail de téléportation :
+            'P' = portail   (x1)
     */
     
     //le tableau tab est déjà initialisé dans une fonction précédente, pas besoin de le faire ici
@@ -33,8 +35,12 @@ void generate_board (card* tab, int size){       //ATTENTION METTRE EN POINTEUR
         for (int j=1 ; j<(size-1) ; j++){
             if (remaining_card > 0){
                 int alea= rand() % remaining_card;
+                // événement
+                if(cards[alea] == 'e'){
+                    (*(tab + i*size + j)).event = 1;
+                }
                 //Les 4 monstres
-                if(cards[alea] == 'b'){
+                else if(cards[alea] == 'b'){
                     (*(tab + i*size + j)).m.basilisk = 1;
                 }
                 else if(cards[alea] == 'z'){
@@ -45,6 +51,13 @@ void generate_board (card* tab, int size){       //ATTENTION METTRE EN POINTEUR
                 }
                 else if(cards[alea] == 'h'){
                     (*(tab + i*size + j)).m.harpy = 1;
+                }
+                // coffre et totem
+                else if(cards[alea] == 'C'){
+                    (*(tab + i*size + j)).treasure = 1;
+                }
+                else if(cards[alea] == 'T'){
+                    (*(tab + i*size + j)).totem = 1;
                 }
                 //Les 4 armes antiques
                 else if(cards[alea] == 'E'){
@@ -59,17 +72,11 @@ void generate_board (card* tab, int size){       //ATTENTION METTRE EN POINTEUR
                 else if(cards[alea] == 'D'){
                     (*(tab + i*size + j)).r.dagger = 1;
                 }
-                // coffre et totem
-                else if(cards[alea] == 'C'){
-                    (*(tab + i*size + j)).treasure = 1;
+                //portail
+                else if(cards[alea] == 'P'){
+                    (*(tab + i*size + j)).portal = 1;
                 }
-                else if(cards[alea] == 'T'){
-                    (*(tab + i*size + j)).totem = 1;
-                }
-                // événement
-                else if(cards[alea] == 'e'){
-                    (*(tab + i*size + j)).event = 1;
-                }
+                
                 else {
                     printf(" OUPS ! Le jeu n'a pas pu être créé. La carte générée aléatoirement n'est pas dans le paquet.\n");
                     exit(1);
@@ -78,10 +85,11 @@ void generate_board (card* tab, int size){       //ATTENTION METTRE EN POINTEUR
                 remaining_card = remaining_card -1;
             }
             else{
-                printf("Toutes les cartes du jeu sont placées !\n");
+                printf("Toutes les cartes du jeu sont placées !\n");                //est affiché 4 fois car il y a 4 cases vides pour mettre les joueurs
             }
         }
-    } 
+    }
+    
 }
     
     
