7829b47b84abe8eaf801f570f5f5a6a39b8f36d6
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-20 17:35:30 +0200

    Update Deplacement.c

--- Deplacement.c
+++ Deplacement.c
@@ -75,3 +75,93 @@ int main() {
     
     return 0;
 }
+
+
+
+
+// ----------------------------------------------------------------------------
+
+
+// game_functions.c
+
+#include <stdio.h>
+
+#define ROWS 6
+#define COLS 6
+
+void displayTable(int table[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", table[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void move(int table[ROWS][COLS], int* posX, int* posY, int direction) {
+    int deltaX = 0;
+    int deltaY = 0;
+    
+    // Determine the movement based on the direction
+    switch (direction) {
+        case 2: // Down
+            deltaX = 1;
+            break;
+        case 8: // Up
+            deltaX = -1;
+            break;
+        case 4: // Left
+            deltaY = -1;
+            break;
+        case 6: // Right
+            deltaY = 1;
+            break;
+        default:
+            printf("Invalid direction!\n");
+            return;
+    }
+    
+    // Calculate the new coordinates
+    int newPosX = *posX + deltaX;
+    int newPosY = *posY + deltaY;
+    
+    // Check if the new coordinates are valid
+    if (newPosX >= 0 && newPosX < ROWS && newPosY >= 0 && newPosY < COLS) {
+        // Move to the new cell
+        *posX = newPosX;
+        *posY = newPosY;
+    } else {
+        printf("Invalid move!\n");
+    }
+}
+
+void playGame() {
+    int table[ROWS][COLS] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25}
+    };
+    
+    int posX = 0; // Initial X coordinate
+    int posY = 0; // Initial Y coordinate
+    
+    displayTable(table);
+    
+    int direction;
+    while (1) {
+        printf("Enter a direction (2: Down, 8: Up, 4: Left, 6: Right): ");
+        scanf("%d", &direction);
+        
+        // Move based on the direction entered by the user
+        move(table, &posX, &posY, direction);
+        displayTable(table);
+    }
+}
+
+int main() {
+    playGame();
+    
+    return 0;
+}
