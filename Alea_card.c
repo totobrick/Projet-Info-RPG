885e5ce594775a72b2016da5193047c4cd32274a
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-21 12:39:52 +0200

    Update Alea_card.c

--- Alea_card.c
+++ Alea_card.c
@@ -9,54 +9,45 @@ void invert_card(card* card1, card* card2){
     card* transit_card = malloc(sizeof(card));
     //transit_card = card1
     (*transit_card).hidden = (*card1).hidden;
-    (*transit_card).treasure = (*card1).treasure;
-    (*transit_card).totem = (*card1).totem;
-    (*transit_card).portal = (*card1).portal;
-    (*transit_card).event = (*card1).event;
+    for (int i=0 ; i<4 ; i++){
+        (*transit_card).type[i] = (*card1).type[i];
+    }
             //monster
-    (*transit_card).m.zombie = (*card1).m.zombie;
-    (*transit_card).m.troll = (*card1).m.troll;
-    (*transit_card).m.harpy = (*card1).m.harpy;
-    (*transit_card).m.basilisk = (*card1).m.basilisk;
+    for (int i=0 ; i<4 ; i++){
+        (*transit_card).m.type[i] = (*card1).m.type[i];
+    }
             //relic
-    (*transit_card).r.stick = (*card1).r.stick;
-    (*transit_card).r.sword = (*card1).r.sword;
-    (*transit_card).r.grimoire = (*card1).r.grimoire;
-    (*transit_card).r.dagger = (*card1).r.dagger;
+    for (int i=0 ; i<4 ; i++){
+        (*transit_card).r.type[i] = (*card1).r.type[i];
+    }
 
     //card1=card2
     (*card1).hidden = (*card2).hidden;
-    (*card1).treasure = (*card2).treasure;
-    (*card1).totem = (*card2).totem;
-    (*card1).portal = (*card2).portal;
-    (*card1).event = (*card2).event;
+    for (int i=0 ; i<4 ; i++){
+        (*card1).type[i] = (*card2).type[i];
+    }
             //monster
-    (*card1).m.zombie = (*card2).m.zombie;
-    (*card1).m.troll = (*card2).m.troll;
-    (*card1).m.harpy = (*card2).m.harpy;
-    (*card1).m.basilisk = (*card2).m.basilisk;
+    for (int i=0 ; i<4 ; i++){
+        (*card1).m.type[i] = (*card2).m.type[i];
+    }
             //relic
-    (*card1).r.stick = (*card2).r.stick;
-    (*card1).r.sword = (*card2).r.sword;
-    (*card1).r.grimoire = (*card2).r.grimoire;
-    (*card1).r.dagger = (*card2).r.dagger;
+    for (int i=0 ; i<4 ; i++){
+        (*card1).r.type[i] = (*card2).r.type[i];
+    }
 
     //card2=transit_card
     (*card2).hidden = (*transit_card).hidden;
-    (*card2).treasure = (*transit_card).treasure;
-    (*card2).totem = (*transit_card).totem;
-    (*card2).portal = (*transit_card).portal;
-    (*card2).event = (*transit_card).event;
+    for (int i=0 ; i<4 ; i++){
+        (*card2).type[i] = (*transit_card).type[i];
+    }
             //monster
-    (*card2).m.zombie = (*transit_card).m.zombie;
-    (*card2).m.troll = (*transit_card).m.troll;
-    (*card2).m.harpy = (*transit_card).m.harpy;
-    (*card2).m.basilisk = (*transit_card).m.basilisk;
+    for (int i=0 ; i<4 ; i++){
+        (*card2).m.type[i] = (*transit_card).m.type[i];
+    }
             //relic
-    (*card2).r.stick = (*transit_card).r.stick;
-    (*card2).r.sword = (*transit_card).r.sword;
-    (*card2).r.grimoire = (*transit_card).r.grimoire;
-    (*card2).r.dagger = (*transit_card).r.dagger;
+    for (int i=0 ; i<4 ; i++){
+        (*card2).r.type[i] = (*transit_card).r.type[i];
+    }
     
     free(transit_card);
 }
