9593b7842210768d0712db0263a1fc407f2e6b87
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-22 08:59:11 +0200

    Update Event.c

--- Event.c
+++ Event.c
@@ -9,7 +9,7 @@ if (event == 1){
     
     printf(" ... /n/n Sérieusement...?/n ");
        
-        const char* rickRollURL = "https://youtu.be/xLGktQmtR5A";  // Lien vers la vidéo de Rick Astley
+        const char* rickRollURL = "https://youtu.be/xLGktQmtR5A";  // Lien vers la vidéo de Rick Astley en 8 bit
     const char* browser = "xdg-open";  // Commande pour ouvrir l'URL avec le navigateur par défaut sur Linux
     // Lancement du navigateur avec l'URL du Rick Roll
     execlp(browser, browser, rickRollURL, NULL);
