cb95d7eab74cd83f4fa5a9dcd655bcb7bf10fc89
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-22 21:41:12 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -1,1 +1,73 @@
-//comment écrire dans un fichier scores.txt en c?
+#include <stdio.h>
+int main(){ //juste pour tester sur place mercredi ou vendrdi
+  
+FILE *fichier = fopen("Score.txt", "a"); //le mode 'a' ajoute du texte à la fin du fichier
+
+  if (fichier == NULL) {
+    printf("Impossible d'ouvrir le fichier.\n");
+    return 1; // Quitter le programme en cas d'erreur
+}le mode 
+fprintf(fichier, " /t  /t /n");  //il faut reflechir pour mettre le nom et rajouter une variable "coup" qui prend +1 à chaque fin de tour. de plus il faut mettre le temps de la partie je crois.
+   fclose(fichier);
+  
+   FILE *fichier = fopen("Score.txt", "r");
+    if (fichier == NULL) {
+        printf("Impossible d'ouvrir le fichier.\n");
+        return 1;
+    }
+
+    int valeur1, valeur2;
+    fscanf(fichier, "%d", &valeur1);
+    fscanf(fichier, "%d", &valeur2);
+
+    if (valeur1 > valeur2) {
+        printf("La valeur 1 est supérieure à la valeur 2.\n");
+    } else if (valeur1 < valeur2) {
+        printf("La valeur 1 est inférieure à la valeur 2.\n");
+    } else {
+        printf("La valeur 1 est égale à la valeur 2.\n");
+  
+  fclose(fichier);
+  return 0;
+}
+  
+/*  
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NOMBRE_DE_VALEURS 100
+#define NOMBRE_DE_VALEURS_A_CONSERVER 10
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
+    FILE *fichier = fopen("nom_du_fichier.txt", "r");
+    if (fichier == NULL) {
+        printf("Impossible d'ouvrir le fichier.\n");
+        return 1;
+    }
+
+    int valeurs[NOMBRE_DE_VALEURS];
+    int plus_petites_valeurs[NOMBRE_DE_VALEURS_A_CONSERVER];
+    int i;
+
+    for (i = 0; i < NOMBRE_DE_VALEURS; i++) {
+        fscanf(fichier, "%d", &valeurs[i]);
+    }
+
+    qsort(valeurs, NOMBRE_DE_VALEURS, sizeof(int), compare);
+
+    for (i = 0; i < NOMBRE_DE_VALEURS_A_CONSERVER; i++) {
+        plus_petites_valeurs[i] = valeurs[i];
+    }
+
+    for (i = 0; i < NOMBRE_DE_VALEURS_A_CONSERVER; i++) {
+        printf("%d\n", plus_petites_valeurs[i]);
+    }
+
+    fclose(fichier);
+    return 0;
+}
+*/
