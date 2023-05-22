b3cb9a83e405a2b60cdad5cdebfef0b3c028993d
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-22 11:25:40 +0200

    Update Ncurses.c

--- Ncurses.c
+++ Ncurses.c
@@ -3,10 +3,13 @@ https://arnaud-feltz.developpez.com/tutoriels/ncurses/?page=premier_pas
 https://www.sbarjatiya.com/notes_wiki/index.php/Using_ncurses_library_with_C
 */
 #include <ncurses.h>
-#include <stdio.h>
 
-int main(void){
-  initscreen();
+int main(){
+  initscr();
   printw("Bonjour!");
+  refresh();
+  getch();
+  endwin();
   
+  return 0;
 }
