628558f90c9f1f8fb3798567f75a7f30c33dff15
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-22 11:38:22 +0200

    Update Ncurses.c

--- Ncurses.c
+++ Ncurses.c
@@ -1,6 +1,9 @@
 /*SOURCES :
 https://arnaud-feltz.developpez.com/tutoriels/ncurses/?page=premier_pas
 https://www.sbarjatiya.com/notes_wiki/index.php/Using_ncurses_library_with_C
+POUR COMPILER :		gcc -o exe_ncurses Nucurses.c -lncurses
+	-> l'extension -lncurses à la fin est essentielle
+*/
 */
 #include <ncurses.h>
 
