6b2d997a52e08fa7430d6fd78b958d76d9cfd7b8
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-22 11:35:39 +0200

    Update Graphisme.c

--- Graphisme.c
+++ Graphisme.c
@@ -1,31 +1,3 @@
-//logo et couleur
-
-
-Voleur
-"T" (code ASCII 84) : Ce caractère peut représenter un voleur de manière stylisée, avec ses bras étendus pour symboliser la discrétion et la rapidité.
-
-"%" (code ASCII 37) : Ce caractère peut représenter un voleur avec son masque ou son bandeau sur le visage, symbolisant l'anonymat et la clandestinité.
-
-"$" (code ASCII 36) : Ce caractère peut être utilisé pour représenter un voleur en référence à l'argent, un élément souvent associé aux activités des voleurs.
-
-"@" (code ASCII 64) : Ce caractère peut représenter un voleur dans une position furtive ou accroupie, prêt à agir rapidement.
-
-
-  Le chevalier :
-
-"Ø" (code ASCII 216) : Ce caractère ressemble à un bouclier, représentant la protection et la défense du chevalier.
-  
-Le mage :
-
-
-"§" (code ASCII 167) : Ce caractère peut représenter un livre de sorts, symbolisant la connaissance et la maîtrise des incantations d'un mage.
-
-  ranger
-
-"®" (code ASCII 174) : Ce caractère peut représenter un ranger avec son emblème ou son insigne, symbolisant son statut et son affiliation.
-
-"Ô" (code ASCII 212) : Ce caractère peut évoquer l'image d'un ranger portant une capuche ou un chapeau, symbole de sa connexion avec la nature.
-
   
 Couleur:
 
