431c555b38c602fc3ecafc36c423a202e77c8a9c
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-22 10:00:03 +0200

    Update Main_toto.c

--- Main_toto.c
+++ Main_toto.c
@@ -1,3 +1,5 @@
+#include "Header.h"
+
 /*_________________________________________________________________________________________________*/
 void board (card* tab, int size){
     init_wall(tab, size);
@@ -11,13 +13,17 @@ int main(){
     srand(time(NULL));
     card* game; 
     game = malloc(SIZE*SIZE*sizeof(card));            //game est notre plateau de jeu (tableau)
+    if (game == NULL){
+        exit(10);
+    }
     board(game, SIZE);
     for (int i=0 ; i<SIZE ; i++){
         printf("\n    Ligne n°%d\n",i);
         for (int j=0 ; j<SIZE ; j++){
             printf("[%d,%d,%d,%d,%d,%d,   %d,%d,%d,%d,   %d,%d,%d,%d]\n",(*(game + i*SIZE + j)).wall, (*(game + i*SIZE + j)).hidden, (*(game + i*SIZE + j)).type[0], (*(game + i*SIZE + j)).type[1], (*(game + i*SIZE + j)).type[2], (*(game + i*SIZE + j)).type[3], (*(game + i*SIZE + j)).m.type[0], (*(game + i*SIZE + j)).m.type[1], (*(game + i*SIZE + j)).m.type[2], (*(game + i*SIZE + j)).m.type[3], (*(game + i*SIZE + j)).r.type[0], (*(game + i*SIZE + j)).r.type[1], (*(game + i*SIZE + j)).r.type[2], (*(game + i*SIZE + j)).r.type[3]);
         }
-  }
+    }
+    free(game);//ATTENTION
   return 0;
 }
 
