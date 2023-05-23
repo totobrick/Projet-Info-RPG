f7a25e7b191f533767bf5ca03561e5f78abf1f8d
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-23 17:13:00 +0200

    Create Menu.c

---
+++ Menu.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void afficherMenu() {
+    int choix;
+
+    do {
+        printf("\nMenu :\n");
+        printf("1. Jouer\n");
+        printf("2. Afficher le scoreboard\n");
+        printf("3. Sortir\n");
+        printf("Choisissez une option : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                printf("Lancement du jeu...\n");
+                // Appeler la fonction pour jouer ici
+                break;
+            case 2:
+                printf("Affichage du scoreboard...\n");
+                // Appeler la fonction pour afficher le scoreboard ici
+                break;
+            case 3:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Option invalide. Veuillez choisir une option valide.\n");
+                break;
+        }
+    } while (choix != 3);
+}
+
+int main() {
+    afficherMenu();
+    return 0;
+}
