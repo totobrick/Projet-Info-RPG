975929b4534ed60f81177099f29ca9f712dfafce
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-23 16:38:59 +0200

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void afficherTexteCentre(const char *texte, int largeur) {
+    int longueur = strlen(texte);
+    int espaces = (largeur - longueur) / 2;
+
+    for (int i = 0; i < espaces; i++) {
+        printf(" ");
+    }
+
+    printf("%s\n", texte);
+}
+
+int main() {
+    char texte[] = "Bienvenue dans CY Memory RPG !";
+    int largeur = 80;
+
+    afficherTexteCentre(texte, largeur);
+
+    return 0;
+}
