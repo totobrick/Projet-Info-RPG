7794cf7beca70d9f07001af5a70365bea37d9bff
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-23 20:12:43 +0200

    Update Ncurses.c

--- Ncurses.c
+++ Ncurses.c
@@ -1,4 +1,5 @@
 /*SOURCES :
+https://www.youtube.com/watch?v=lV-OPQhPvSM
 https://arnaud-feltz.developpez.com/tutoriels/ncurses/?page=premier_pas 
 https://www.sbarjatiya.com/notes_wiki/index.php/Using_ncurses_library_with_C
 
