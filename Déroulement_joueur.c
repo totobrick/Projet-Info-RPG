f887d69fbaff5f1fd45e7a0f7f076ac1dcf97697
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-24 12:03:42 +0200

    Update Déroulement_joueur.c

--- Déroulement_joueur.c
+++ Déroulement_joueur.c
@@ -14,78 +14,78 @@ int main {
   int verif = 0;
   int weapon=0;
   int enemy=0;
-  int direction;  
+  int direction;
+  int number_of_players_who_played=0;
+  int number_of_players=0;
   Player P;
   card c;
   
-  while (P.life==1 && P.relic==0 || P.treasure==0){
+ while(number_of_players<number_of_players_who_played){
+     while (P.life==1 && P.relic==0 || P.treasure==0){
   
 /*---------------------------------- ETAPE 1 --------------------------------------*/
                    
-    printf("Choisissez votre arme : \n");
-    printf("1. Bouclier\n");
-    printf("2. Torche\n");
-    printf("3. Hache\n");
-    printf("4. Arc\n");
+        printf("Choisissez votre arme : \n");
+        printf("1. Bouclier\n");
+        printf("2. Torche\n");
+        printf("3. Hache\n");
+        printf("4. Arc\n");
     
     
-    do {
-        printf("Votre choix: ");
-        verif = scanf("%d", &weapon);
-        vide_buffer();
-    } while (weapon != 1 && weapon != 2 && weapon != 3 && weapon != 4 || verif != 1);
-verif=0;
+        do {
+            printf("Votre choix: ");
+            verif = scanf("%d", &weapon);
+            vide_buffer();
+        } while (weapon != 1 && weapon != 2 && weapon != 3 && weapon != 4 || verif != 1);
+        verif=0;
 
 
-    if (weapon == 1){               //bouclier
-        P.w.type[0] = 1;
-        P.w.type[1] = 0;
-        P.w.type[2] = 0;
-        P.w.type[3] = 0;
-    }
+        if (weapon == 1){               //bouclier
+            P.w.type[0] = 1;
+            P.w.type[1] = 0;
+            P.w.type[2] = 0;
+            P.w.type[3] = 0;
+        }
     
     
-    else if (weapon == 2){          //Torche
-        P.w.type[0] = 0;
-        P.w.type[1] = 1;
-        P.w.type[2] = 0;
-        P.w.type[3] = 0;
-    }
+        else if (weapon == 2){          //Torche
+            P.w.type[0] = 0;
+            P.w.type[1] = 1;
+            P.w.type[2] = 0;
+            P.w.type[3] = 0;
+        }
     
     
-    else if (weapon == 3){          //hache
-        P.w.type[0] = 0;
-        P.w.type[1] = 0;
-        P.w.type[2] = 1;
-        P.w.type[3] = 0;
-    }
+        else if (weapon == 3){          //hache
+            P.w.type[0] = 0;
+            P.w.type[1] = 0;
+            P.w.type[2] = 1;
+            P.w.type[3] = 0;
+        }
     
     
-    else{                           //arc
-        P.w.type[0] = 0;
-        P.w.type[1] = 0;
-        P.w.type[2] = 0;
-        P.w.type[3] = 1;   
-    }
+        else{                           //arc
+            P.w.type[0] = 0;
+            P.w.type[1] = 0;
+            P.w.type[2] = 0;
+            P.w.type[3] = 1;   
+        }
     
 /*---------------------------------- ETAPE 2 --------------------------------------*/
-    printf("Enter a direction (2: Down, 8: Up, 4: Left, 6: Right): ");
-    scanf("%d", &direction);
-    move(table, &posX, &posY, direction);
+        printf("Enter a direction (2: Down, 8: Up, 4: Left, 6: Right): ");
+        scanf("%d", &direction);
+        move(table, &posX, &posY, direction);
        
  /*---------------------------------- ETAPE 3 --------------------------------------*/
-    if (c.m==1){
-       combat(P,c)
-       }
+        if (c.m==1){
+            combat(P,c)
+        }
       
-      
-      
-      
-      
-  } // while
-
-
-
+        P.move++
+      } // while joueur
+     
+number_of_players_who_played++
+} //while manche
 
 
 } // main
