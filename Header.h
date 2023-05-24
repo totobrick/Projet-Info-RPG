7aea1095f77eb6d4225a3b5c5bf8a53f60965822
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-24 10:38:08 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -9,10 +9,10 @@
 typedef struct{
     int type[4];
         /*
-        type[0] = torch        (1=oui 0=non)                           <- 1.zombie
-        type[1] = axe         (1=oui 0=non)                           <- 2.troll
-        type[2] = bow         (1=oui 0=non)                           <- 3.harpy
-        type[3] = shield       (1=oui 0=non)                          <- 4.basilisk
+        type[0] = shield       (1=oui 0=non)                           <- 1.zombie
+        type[1] =  torch       (1=oui 0=non)                           <- 2.troll
+        type[2] =   axe        (1=oui 0=non)                           <- 3.harpy
+        type[3] =   bow        (1=oui 0=non)                          <- 4.basilisk
         */ 
 } weapons;
 
