a71fb5e7763e6d033c5664d7e298aae3e0bfd189
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-24 16:55:46 +0200

    Update Portal.c

--- Portal.c
+++ Portal.c
@@ -1,18 +1,22 @@
 #include "Header.h"
 
-void (Player P, card c){
+void Portal (Player P, card c){
  int new_direction_x = 0;
   int new_direction_y = 0;
   do{
   printf("Choisissez les coordonnés x, entre 1 et 6, vers lesquels vous voulez allez!");
   verif = scanf ("%d", &new_direction_x);
   vide_buffer();
-  }while ( verif != 1 && new_direction_x<1 && new_direction>6);
-  
+  }while ( verif != 1 && new_direction_x<1 && new_direction_x>6);
+ verif=0; 
+ 
  do{
   printf("Choisissez les coordonnés y, entre 1 et 6, vers lesquels vous voulez allez!");
-  verif = scanf ("%d", &new_direction);
+  verif = scanf ("%d", &new_direction_y);
   vide_buffer();
-  }while ( verif != 1 && new_direction<1 && new_direction>6);
+  }while ( verif != 1 && new_direction_y<1 && new_direction_y>6);
+ verif=0
+ 
   
+ 
 }
