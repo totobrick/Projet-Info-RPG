50d58433ca3783e8ef4ea467b646d9e8953946fa
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-24 17:39:29 +0200

    Update Totem.c

--- Totem.c
+++ Totem.c
@@ -1,4 +1,26 @@
 #include "Header.h"
+extern int Totem1;
+extern int Totem2;
+
+void teleport(Player P, card c) {
+
+    // Vérification si le joueur est sur l'un des totems
+    if (c.P) {
+        // Téléportation vers le deuxième totem
+        grid[totem1_x][totem1_y] = 0;
+        grid[totem2_x][totem2_y] = 2;
+        printf("Le joueur a été téléporté vers le deuxième totem.\n");
+    } else if (player_x == totem2_x && player_y == totem2_y) {
+        // Téléportation vers le premier totem
+        grid[totem2_x][totem2_y] = 0;
+        grid[totem1_x][totem1_y] = 2;
+        printf("Le joueur a été téléporté vers le premier totem.\n");
+    } else {
+        printf("Le joueur n'est pas sur l'un des totems, aucune téléportation n'a eu lieu.\n");
+    }
+}
+
+
 
 int main {
 
