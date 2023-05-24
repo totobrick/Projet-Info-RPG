63daa940b7c7e029fe39b121aaeda3c443749de9
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-24 10:57:26 +0200

    Update Déroulement_joueur.c

--- Déroulement_joueur.c
+++ Déroulement_joueur.c
@@ -1,8 +1,21 @@
 #include "Header.h"
-Player P
+
+
+void vide_buffer() {
+    int c;
+    while ((c = getchar()) != '\n') {
+    }
+
 
 int main {
-
+  
+  int e1 = 1, e2 = 1, e3 = 1, e4 = 1, e5 = 1, e6 = 1, e7 = 1, e8 = 1, e9 = 1, e10 = 1, e11 = 1;
+  int verif = 0;
+  int weapon=0;
+  int enemy=0;
+  Player P;
+  card c;
+  
   while (P.life==1 && P.relic==0 || P.treasure==0){
   
 /*----------------------------------ETAPE 1--------------------------------------*/
@@ -52,11 +65,16 @@ verif=0;
         P.w.type[2] = 0;
         P.w.type[3] = 1;   
     }
+    
   
-  }
+  
+  
+  
+  
+  } // while
 
 
 
 
 
-}
+} // main
