f1ac9ca3d5436349e76ef98087cfa805dc46df4a
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-24 17:31:39 +0200

    Update Event.c

--- Event.c
+++ Event.c
@@ -4,7 +4,7 @@ int e1 = 1, e2 = 1, e3 = 1, e4 = 1, e5 = 1, e6 = 1, e7 = 1, e8 = 1, e9 = 1, e10 
 Player P;
 card c;
 
-void(card* c, Player* P){
+void event(card* c, Player* P){
 
 //if (event == 1){
     do{
