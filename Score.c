67f5e062bf919a28c9924af2939222ceef935d9b
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-24 17:52:08 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -33,46 +33,26 @@ FILE *fichier = fopen("Score.txt", "a"); //le mode 'a' ajoute du texte à la fin
 }
   
 /*  
-#include <stdio.h>
-#include <stdlib.h>
+char ligne[100]; // Choisir une taille appropriée pour stocker une ligne du fichier
+int numeroLigne = 3; // Exemple : numéro de la ligne à réécrire
 
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
+// Parcourir le fichier jusqu'à la ligne souhaitée
+int i = 1;
+while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+    if (i == numeroLigne) {
+        // La ligne spécifique est trouvée, vous pouvez la réécrire
+        fseek(fichier, -strlen(ligne), SEEK_CUR); // Positionne le curseur avant la ligne
+        fprintf(fichier, "Nouvelle ligne à écrire\n"); // Écrit la nouvelle ligne
+        break; // Sort de la boucle une fois la ligne réécrite
+    }
+    i++;
 }
 
-int main() {
-    FILE *fichier = fopen("nom_du_fichier.txt", "r");
-    if (fichier == NULL) {
-        printf("Impossible d'ouvrir le fichier.\n");
-        return 1;
-    }
-
-    int *valeurs = NULL;
-    int taille_tableau = 0;
-    int capacite_tableau = 10;
-    int i;
-
-    while (!feof(fichier)) {
-        int valeur;
-        fscanf(fichier, "%d", &valeur);
-
-        if (taille_tableau >= capacite_tableau) {
-            capacite_tableau *= 2;
-            valeurs = realloc(valeurs, capacite_tableau * sizeof(int));
-        }
-
-        valeurs[taille_tableau] = valeur;
-        taille_tableau++;
-    }
-
-    qsort(valeurs, taille_tableau, sizeof(int), compare);
-
-    for (i = 0; i < 10 && i < taille_tableau; i++) {
-        printf("%d\n", valeurs[i]);
-    }
-
-    free(valeurs);
-    fclose(fichier);
-    return 0;
+// Si la ligne spécifique n'est pas trouvée, vous pouvez afficher un message d'erreur
+if (i > numeroLigne) {
+    printf("La ligne spécifique n'a pas été trouvée.\n");
 }
+
+// Fermez le fichier une fois que vous avez terminé les opérations
+fclose(fichier);
 */
