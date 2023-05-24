53f6226521c49bd25e76c22c5b368e4322e056a3
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-24 11:09:59 +0200

    Update Deplacement.c

--- Deplacement.c
+++ Deplacement.c
@@ -1,94 +1,6 @@
 #include "Header.h"
-
-#define ROWS 6
-#define COLS 6
-
-void afficherTableau(int tableau[ROWS][COLS]) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%d ", tableau[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void deplacer(int tableau[ROWS][COLS], int* posX, int* posY, int direction) {
-    int deltaX = 0;
-    int deltaY = 0;
-    
-    // Déterminer le déplacement en fonction de la direction
-    switch (direction) {
-        case 2: // Bas
-            deltaX = 1;
-            break;
-        case 8: // Haut
-            deltaX = -1;
-            break;
-        case 4: // Gauche
-            deltaY = -1;
-            break;
-        case 6: // Droite
-            deltaY = 1;
-            break;
-        default:
-            printf("Direction invalide !\n");
-            return;
-    }
-    
-    // Calculer les nouvelles coordonnées
-    int newPosX = *posX + deltaX;
-    int newPosY = *posY + deltaY;
-    
-    // Vérifier si les nouvelles coordonnées sont valides
-    if (newPosX >= 0 && newPosX < ROWS && newPosY >= 0 && newPosY < COLS) {
-        // Déplacer vers la nouvelle case
-        *posX = newPosX;
-        *posY = newPosY;
-    } else {
-        printf("Déplacement impossible !\n");
-    }
-}
-
-int main() {
-    int tableau[ROWS][COLS] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20},
-        {21, 22, 23, 24, 25}
-    };
-    
-    int posX = 0; // Coordonnée X initiale
-    int posY = 0; // Coordonnée Y initiale
-    
-    afficherTableau(tableau);
-    
-    int direction;
-    while (1) {
-        printf("Entrez une direction (2: Bas, 8: Haut, 4: Gauche, 6: Droite) : ");
-        scanf("%d", &direction);
-        
-        // Déplacer en fonction de la direction entrée par l'utilisateur
-        deplacer(tableau, &posX, &posY, direction);
-        afficherTableau(tableau);
-    }
-    
-    return 0;
-}
-
-
-
-
-// ----------------------------------------------------------------------------
-
-
-// game_functions.c
-
 #include <stdio.h>
-
-#define ROWS 6
-#define COLS 6
-
+/*
 void displayTable(int table[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
@@ -97,7 +9,7 @@ void displayTable(int table[ROWS][COLS]) {
         printf("\n");
     }
 }
-
+*/
 void move(int table[ROWS][COLS], int* posX, int* posY, int direction) {
     int deltaX = 0;
     int deltaY = 0;
@@ -133,35 +45,22 @@ void move(int table[ROWS][COLS], int* posX, int* posY, int direction) {
     } else {
         printf("Invalid move!\n");
     }
-}
+//}
 
-void playGame() {
-    int table[ROWS][COLS] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20},
-        {21, 22, 23, 24, 25}
-    };
-    
+//void playGame() 
+    /*
     int posX = 0; // Initial X coordinate
     int posY = 0; // Initial Y coordinate
+    */
     
     displayTable(table);
     
     int direction;
-    while (1) {
         printf("Enter a direction (2: Down, 8: Up, 4: Left, 6: Right): ");
         scanf("%d", &direction);
         
         // Move based on the direction entered by the user
         move(table, &posX, &posY, direction);
-        displayTable(table);
-    }
-}
+        //displayTable(table);
+//}
 
-int main() {
-    playGame();
-    
-    return 0;
-}
