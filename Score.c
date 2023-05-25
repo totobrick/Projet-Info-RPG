9e3bd2f1e051476aea6416b645da58f197542940
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-25 11:26:03 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -1,67 +1,19 @@
 #include "Header.h"
 int main(){ //juste pour tester sur place mercredi ou vendrdi
-  
-FILE *fichier = fopen("Score.txt", "a"); //le mode 'a' ajoute du texte à la fin du fichier
+  void Score_creator(Player P){
+FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fin du fichier
 
   if (fichier == NULL) {
-    printf("Impossible d'ouvrir le fichier.\n");
+    printf("Erreur\n");
     return 1; // Quitter le programme en cas d'erreur
 } 
 
-    fprintf(fichier, " /t  /t /n");  //il faut reflechir pour mettre le nom et rajouter une variable "coup" qui prend +1 à chaque fin de tour. de plus il faut mettre le temps de la partie je crois.
-   fclose(fichier);
-  
-   FILE *fichier = fopen("Score.txt", "r");
-    if (fichier == NULL) {
-        printf("Impossible d'ouvrir le fichier.\n");
-        return 1;
-    }
-
-    int valeur1, valeur2;
-    fscanf(fichier, "%d", &valeur1);
-    fscanf(fichier, "%d", &valeur2);
-
-    if (valeur1 > valeur2) {
-        printf("La valeur 1 est supérieure à la valeur 2.\n");
-    } else if (valeur1 < valeur2) {
-        printf("La valeur 1 est inférieure à la valeur 2.\n");
-    } else {
-        printf("La valeur 1 est égale à la valeur 2.\n");
-  
+    fprintf(fichier, "%s /t %d /t %d /t %d /t %d /t %d",  P.nom, P.slay, P.chest, P.reversed, P.Score_victory, P.move); 
+    
   fclose(fichier);
   return 0;
 }
   
-/*  
-char ligne[100]; // Choisir une taille appropriée pour stocker une ligne du fichier
-int numeroLigne = 3; // Exemple : numéro de la ligne à réécrire
-
-// Parcourir le fichier jusqu'à la ligne souhaitée
-int i = 1;
-while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-    if (i == numeroLigne) {
-        // La ligne spécifique est trouvée, vous pouvez la réécrire
-        fseek(fichier, -strlen(ligne), SEEK_CUR); // Positionne le curseur avant la ligne
-        fprintf(fichier, "Nouvelle ligne à écrire\n"); // Écrit la nouvelle ligne
-        break; // Sort de la boucle une fois la ligne réécrite
-    }
-    i++;
-}
-
-// Si la ligne spécifique n'est pas trouvée, vous pouvez afficher un message d'erreur
-if (i > numeroLigne) {
-    printf("La ligne spécifique n'a pas été trouvée.\n");
-}
-
-// Fermez le fichier une fois que vous avez terminé les opérations
-fclose(fichier);
-*/
-
-  
-  #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 typedef struct {
     int id;
     char nom[50];
@@ -101,19 +53,14 @@ void mettreAJourSalaire(FILE* fichier, int id, float nouveauSalaire) {
 }
 
 int main() {
-    FILE* fichier = fopen("donnees.txt", "a+");
-    if (fichier == NULL) {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-        return 1;
-    }
 
     // Exemple d'utilisation : ajout d'employés et mise à jour de salaire
-    ajouterEmploye(fichier, 1, "John Doe", 5000.0);
-    ajouterEmploye(fichier, 2, "Jane Smith", 6000.0);
-    ajouterEmploye(fichier, 3, "Bob Johnson", 5500.0);
+    addPlayer(fichier, 1, "John Doe", 5000.0);
+    addPlayer(fichier, 2, "Jane Smith", 6000.0);
+    addPlayer(fichier, 3, "Bob Johnson", 5500.0);
 
-    mettreAJourSalaire(fichier, 2, 6500.0);
-    mettreAJourSalaire(fichier, 4, 7000.0); // L'employé avec l'ID 4 n'existe pas, donc aucun changement
+    miseAjour(fichier, 2, 6500.0);
+    miseAjour(fichier, 4, 7000.0); // L'employé avec l'ID 4 n'existe pas, donc aucun changement
 
     // Lecture du fichier pour afficher les employés et leurs salaires mis à jour
     rewind(fichier); // Rembobiner le fichier au début
@@ -130,3 +77,4 @@ int main() {
 
     return 0;
 }
+} //void
