26751db2e9ea549aa76aff86df4a28e5909f9341
Draxas95 <64579762+Draxas95@users.noreply.github.com>
2023-05-25 10:48:09 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -56,3 +56,77 @@ if (i > numeroLigne) {
 // Fermez le fichier une fois que vous avez terminé les opérations
 fclose(fichier);
 */
+
+  
+  #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char nom[50];
+    float salaire;
+} Employe;
+
+void ajouterEmploye(FILE* fichier, int id, const char* nom, float salaire) {
+    fprintf(fichier, "%d %s %.2f\n", id, nom, salaire);
+}
+
+void mettreAJourSalaire(FILE* fichier, int id, float nouveauSalaire) {
+    FILE* fichierTemp = fopen("temp.txt", "w");
+    if (fichierTemp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier temporaire.\n");
+        return;
+    }
+
+    char ligne[100];
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        int idFichier;
+        char nomFichier[50];
+        float salaireFichier;
+        sscanf(ligne, "%d %s %f", &idFichier, nomFichier, &salaireFichier);
+
+        if (idFichier == id) {
+            fprintf(fichierTemp, "%d %s %.2f\n", id, nomFichier, nouveauSalaire);
+        } else {
+            fprintf(fichierTemp, "%d %s %.2f\n", idFichier, nomFichier, salaireFichier);
+        }
+    }
+
+    fclose(fichier);
+    fclose(fichierTemp);
+
+    remove("donnees.txt");
+    rename("temp.txt", "donnees.txt");
+}
+
+int main() {
+    FILE* fichier = fopen("donnees.txt", "a+");
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        return 1;
+    }
+
+    // Exemple d'utilisation : ajout d'employés et mise à jour de salaire
+    ajouterEmploye(fichier, 1, "John Doe", 5000.0);
+    ajouterEmploye(fichier, 2, "Jane Smith", 6000.0);
+    ajouterEmploye(fichier, 3, "Bob Johnson", 5500.0);
+
+    mettreAJourSalaire(fichier, 2, 6500.0);
+    mettreAJourSalaire(fichier, 4, 7000.0); // L'employé avec l'ID 4 n'existe pas, donc aucun changement
+
+    // Lecture du fichier pour afficher les employés et leurs salaires mis à jour
+    rewind(fichier); // Rembobiner le fichier au début
+    char ligne[100];
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        int id;
+        char nom[50];
+        float salaire;
+        sscanf(ligne, "%d %s %f", &id, nom, &salaire);
+        printf("ID : %d, Nom : %s, Salaire : %.2f\n", id, nom, salaire);
+    }
+
+    fclose(fichier);
+
+    return 0;
+}
