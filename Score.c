2164b191e3cf9c5c973ec17f3bf7f85dbf219132
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-25 12:07:59 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -1,5 +1,6 @@
 #include "Header.h"
 int main(){ //juste pour tester sur place mercredi ou vendrdi
+  
   void Score_creator(Player P){
 FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fin du fichier
 
@@ -7,24 +8,16 @@ FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fi
     printf("Erreur\n");
     return 1; // Quitter le programme en cas d'erreur
 } 
-
-    fprintf(fichier, "%s /t %d /t %d /t %d /t %d /t %d",  P.nom, P.slay, P.chest, P.reversed, P.Score_victory, P.move); 
+     updateScore(fichier, Player P);
+    
+      fprintf(fichier, "%s /t %d /t %d /t %d /t %d /t %d /t %d /n",  P.nom, P.class, P.move, P.reversed, P.slay, P.chest, P.Score_victory);
+    
     
   fclose(fichier);
-  return 0;
-}
-  
-typedef struct {
-    int id;
-    char nom[50];
-    float salaire;
-} Employe;
+ // return 0;
+  }
 
-void ajouterEmploye(FILE* fichier, int id, const char* nom, float salaire) {
-    fprintf(fichier, "%d %s %.2f\n", id, nom, salaire);
-}
-
-void mettreAJourSalaire(FILE* fichier, int id, float nouveauSalaire) {
+void updateScore(FILE* fichier, Player P) {
     FILE* fichierTemp = fopen("temp.txt", "w");
     if (fichierTemp == NULL) {
         printf("Erreur lors de l'ouverture du fichier temporaire.\n");
@@ -54,13 +47,8 @@ void mettreAJourSalaire(FILE* fichier, int id, float nouveauSalaire) {
 
 int main() {
 
-    // Exemple d'utilisation : ajout d'employés et mise à jour de salaire
-    addPlayer(fichier, 1, "John Doe", 5000.0);
-    addPlayer(fichier, 2, "Jane Smith", 6000.0);
-    addPlayer(fichier, 3, "Bob Johnson", 5500.0);
-
-    miseAjour(fichier, 2, 6500.0);
-    miseAjour(fichier, 4, 7000.0); // L'employé avec l'ID 4 n'existe pas, donc aucun changement
+   
+    
 
     // Lecture du fichier pour afficher les employés et leurs salaires mis à jour
     rewind(fichier); // Rembobiner le fichier au début
