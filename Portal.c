ab0a1f026bb5309222a69fe897a3eb1a4a251ed3
totobrick <133356452+totobrick@users.noreply.github.com>
2023-05-25 18:01:53 +0200

    Update Portal.c

--- Portal.c
+++ Portal.c
@@ -10,14 +10,14 @@ void Portal (Player* P, card* tab, int size){
             printf("Choisissez les coordonnés x, entre 1 et 6, vers lesquels vous voulez allez!");
             verif = scanf ("%d", &new_direction_x);
             vide_buffer();
-        }while ( verif != 1 && new_direction_x<1 && new_direction_x>6);
+        }while ( verif != 1 || new_direction_x<1 || new_direction_x>6);
         verif=0; 
 
         do{
             printf("Choisissez les coordonnés y, entre 1 et 6, vers lesquels vous voulez allez!");
             verif = scanf ("%d", &new_direction_y);
             vide_buffer();
-        }while ( verif != 1 && new_direction_y<1 && new_direction_y>6);
+        }while ( verif != 1 || new_direction_y<1 || new_direction_y>6);
         verif=0
 
         card* new_card = tab + new_direction_y*size + new_direction_x;
