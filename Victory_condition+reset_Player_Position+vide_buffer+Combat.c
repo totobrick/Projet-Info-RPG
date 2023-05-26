b8d8f7f61abb1939edaaf2830b58c6e450cb2641
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-26 13:44:53 +0200

    Update and rename Combat.c to Victory_condition+reset_Player_Position+vide_buffer+Combat.c

--- Victory_condition+reset_Player_Position+vide_buffer+Combat.c
+++ Victory_condition+reset_Player_Position+vide_buffer+Combat.c
@@ -56,3 +56,21 @@ void combat(Player P, card c) {
     }
     }
     }
+
+
+
+// Fonction pour vérifier si le joueur a obtenu le trésor et la relique 
+
+int checkTreasure(Player* P) {
+    int victory = 0;
+    if (P.treasure == 1 && P.relic == 1) {
+                victory = 1;
+
+}
+    else{
+     victory = 0;   
+    }
+
+    return victory;
+}
+
