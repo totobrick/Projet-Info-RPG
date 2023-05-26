640e759df7324cac26d1b4048d0eefd03ebffd84
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-26 13:42:57 +0200

    Update Victory_condition.c

--- Victory_condition.c
+++ Victory_condition.c
@@ -1,18 +1,1 @@
-#include "Header.h"
-
-
-// Fonction pour vérifier si le joueur a obtenu le trésor et la relique 
-
-int checkTreasure(Player* P) {
-    int victory = 0;
-    if (P.treasure == 1 && P.relic == 1) {
-                victory = 1;
-
-}
-    else{
-     victory = 0;   
-    }
-
-    return victory;
-}
 
