9bb127f4a2231ea34d48b68e45fda3fb598ba1e5
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-26 10:53:41 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -2,51 +2,65 @@
   
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-    FILE *fichier;
-
-    // Ouverture du fichier en mode lecture
-    fichier = fopen("donnees.txt", "r");
-
-    if (fichier == NULL) {
-        printf("Erreur lors de l'ouverture du fichier.\n");
+  
+void updateScore(FILE* fichier, Player* P,char job []) {
+    FILE* fichierTemp = fopen("temp.txt", "w");
+    if (fichierTemp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier temporaire.\n");
         return 1;
     }
-
-    // Détermination de la taille du fichier
-    fseek(fichier, 0, SEEK_END);
-    long taille_fichier = ftell(fichier);
-
-    // Vérification si le fichier est vide
-    if (taille_fichier == 0) {
-        printf("Le fichier est vide.\n");
-    } else {
-        printf("Le fichier n'est pas vide.\n");
+        char player_name[100];
+        char player_class[10];
+        player_class = job;
+        int player_move;
+        int player_reversed;
+        int player_slay;
+        int player_chest;
+        int player_score_victory;
+    char ligne[100];
+  rewind(fichier);
+ 
+  while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        
+    sscanf(ligne, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+    
+        if (player_name == (*P).name) {
+         player_move += (*P).move;
+          player_reversed += (*P).reversed;
+          player_slay += (*P).slay;
+          player_chest += (*P).chest;
+          player_score_victory += (*P).score_victory;
+           
+            fprintf(fichierTemp,  "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+        } 
+    else {
+            fprintf(fichier, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  (*P).nom, job, (*P).move, (*P).reversed, (*P).slay, (*P).chest, (*P).Score_victory);
     }
-
-    // Fermeture du fichier
+  }
     fclose(fichier);
+    fclose(fichierTemp);
+
+    remove("Score.txt");
+    rename("temp.txt", "Score.txt");
+}
 
 
-
-  void Score_creator(Player P){
+  void Score_creator(Player* P){
 FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fin du fichier
 
   if (fichier == NULL) {
     printf("Erreur\n");
     return 1; // Quitter le programme en cas d'erreur
 } 
-    
-    if (P.class == 1){
+    if ((*P).class == 1){
      char job [10];
         strcpy(job, "Magicien");
     }
-   else if (P.class == 2){
+   else if ((*P).class == 2){
      char job [10];
         strcpy(job, "Guerrier");
     }
-    else if (P.class == 3){
+    else if ((*P).class == 3){
      char job [10];
         strcpy(job, "Ranger");
     }
@@ -55,66 +69,16 @@ FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fi
         strcpy(job, "Voleur");
     }
     
-    
-     updateScore(fichier, Player P, job);
-    
-      fprintf(fichier, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  P.nom, job, P.move, P.reversed, P.slay, P.chest, P.Score_victory);
-    
+    // Détermination de la taille du fichier
+    fseek(fichier, 0, SEEK_END);
+    long file_weight = ftell(fichier);
+
+    // Vérification si le fichier est vide
+    if (file_weight == 0) {
+       fprintf(fichier, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  (*P).nom, job, (*P).move, (*P).reversed, (*P).slay, (*P).chest, (*P).Score_victory);
+    } else {
+        updateScore(fichier, P, job);
+    }
   fclose(fichier);
+  }
 
-void updateScore(FILE* fichier, Player P,job ) {
-    FILE* fichierTemp = fopen("temp.txt", "w");
-    if (fichierTemp == NULL) {
-        printf("Erreur lors de l'ouverture du fichier temporaire.\n");
-        return;
-    }
-        char player_name[100];
-
-        char player_class[10];
-
-        int player_move;
-
-        int player_reversed;
-
-        int player_slay;
-
-        int player_chest;
-
-        int player_score_victory;
-    char ligne[100];
-  rewind(fichier);
-    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        
-    sscanf(ligne, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
-    
-        if (player_name == P.name) {
-         player_move += P.move;
-          player_slay += P.slay;
-          player_chest += P.
-           
-            fprintf(fichierTemp, "%d %s %.2f\n", id, nomFichier, nouveauSalaire);
-        } else {
-            fprintf(fichierTemp, "%d %s %.2f\n", idFichier, nomFichier, salaireFichier);
-        }
-    }
-
-    fclose(fichier);
-    fclose(fichierTemp);
-
-    remove("Score.txt");
-    rename("temp.txt", "Score.txt");
-}
-
-    // Lecture du fichier pour afficher les employés et leurs salaires mis à jour
-    rewind(fichier); // Rembobiner le fichier au début
-    char ligne[100];
-    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        int id;
-        char nom[50];
-        float salaire;
-        sscanf(ligne, "%d %s %f", &id, nom, &salaire);
-        printf("ID : %d, Nom : %s, Salaire : %.2f\n", id, nom, salaire);
-    }
-    return 0;
-}
-} //void
