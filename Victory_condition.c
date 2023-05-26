c356dc23cc61fcc2baf63301a87bf154d817825c
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-26 13:42:34 +0200

    Update Victory_condition.c

--- Victory_condition.c
+++ Victory_condition.c
@@ -1,32 +1,18 @@
 #include "Header.h"
 
 
-// Fonction pour vérifier si le joueur a obtenu le trésor et la relique correspondant à sa classe
-int checkTreasure(Player player) {
-    int victory = 0;
+// Fonction pour vérifier si le joueur a obtenu le trésor et la relique 
 
-    switch (player.class) {
-        case 1: // Guerrier
-            if (player.treasure == 1 && player.relic == 1) {
+int checkTreasure(Player* P) {
+    int victory = 0;
+    if (P.treasure == 1 && P.relic == 1) {
                 victory = 1;
-            }
-            break;
-        case 2: // Ranger
-            if (player.treasure == 1 && player.relic == 1) {
-                victory = 1;
-            }
-            break;
-        case 3: // Magicien
-            if (player.treasure == 1 && player.relic == 1) {
-                victory = 1;
-            }
-            break;
-        case 4: // Voleur
-            if (player.treasure == 1 && player.relic == 1) {
-                victory = 1;
-            }
-            break;
+
+}
+    else{
+     victory = 0;   
     }
 
     return victory;
 }
+
