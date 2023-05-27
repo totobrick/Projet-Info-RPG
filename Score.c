50dc112350239071d4fd082565b816f631671a59
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-27 17:27:51 +0200

    Update Score.c

--- Score.c
+++ Score.c
@@ -1,39 +1,40 @@
 #include "Header.h"
-void updateScore(FILE* fichier, Player* P,char job []) {
+
+void updateScore(FILE* fichier, Player* P, char job[]) {
     FILE* fichierTemp = fopen("temp.txt", "w");
     if (fichierTemp == NULL) {
         printf("Erreur lors de l'ouverture du fichier temporaire.\n");
-        return 1;
+        return;
     }
-        char player_name[100];
-        char player_class[10];
-        player_class = job;
-        int player_move;
-        int player_reversed;
-        int player_slay;
-        int player_chest;
-        int player_score_victory;
+
+    char player_name[100];
+    char player_class[10];
+    strcpy(player_class, job);
+    int player_move;
+    int player_reversed;
+    int player_slay;
+    int player_chest;
+    int player_score_victory;
     char ligne[100];
-  rewind(fichier);
- 
-  while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        
-    sscanf(ligne, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
-    
-        if (player_name == (*P).name) {
-         player_move += (*P).move;
-          player_reversed += (*P).reversed;
-          player_slay += (*P).slay;
-          player_chest += (*P).chest;
-          player_score_victory += (*P).score_victory;
-           
-            fprintf(fichierTemp,  "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
-        }  
-  }
   
-   if(player_name != (*P).name){
-            fprintf(fichier, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  (*P).nom, job, (*P).move, (*P).reversed, (*P).slay, (*P).chest, (*P).Score_victory);
+    rewind(fichier);
+  
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        sscanf(ligne, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, &player_move, &player_reversed, &player_slay, &player_chest, &player_score_victory);
+
+        if (strcmp(player_name, (*P).name) == 0) {
+            player_move += (*P).move;
+            player_reversed += (*P).reversed;
+            player_slay += (*P).slay;
+            player_chest += (*P).chest;
+            player_score_victory += (*P).score_victory;
+
+            fprintf(fichierTemp, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+        } else {
+            fprintf(fichierTemp, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+        }
     }
+
     fclose(fichier);
     fclose(fichierTemp);
 
@@ -41,41 +42,33 @@ void updateScore(FILE* fichier, Player* P,char job []) {
     rename("temp.txt", "Score.txt");
 }
 
+void Score_creator(Player* P) {
+    FILE* fichier = fopen("Score.txt", "a+");
 
-  void Score_creator(Player* P){
-FILE *fichier = fopen("Score.txt", "a+"); //le mode 'a' ajoute du texte à la fin du fichier
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        return;
+    }
 
-  if (fichier == NULL) {
-    printf("Erreur\n");
-    return 1; // Quitter le programme en cas d'erreur
-} 
-    if ((*P).class == 1){
-     char job [10];
+    char job[10];
+    if ((*P).class == 1) {
         strcpy(job, "Magicien");
-    }
-   else if ((*P).class == 2){
-     char job [10];
+    } else if ((*P).class == 2) {
         strcpy(job, "Guerrier");
-    }
-    else if ((*P).class == 3){
-     char job [10];
+    } else if ((*P).class == 3) {
         strcpy(job, "Ranger");
-    }
-    else{
-     char job [10];
+    } else {
         strcpy(job, "Voleur");
     }
-    
-    // Détermination de la taille du fichier
+
     fseek(fichier, 0, SEEK_END);
     long file_weight = ftell(fichier);
 
-    // Vérification si le fichier est vide
     if (file_weight == 0) {
-       fprintf(fichier, "%s /t %s /t %d /t %d /t %d /t %d /t %d /n",  (*P).nom, job, (*P).move, (*P).reversed, (*P).slay, (*P).chest, (*P).Score_victory);
+        fprintf(fichier, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", (*P).name, job, (*P).move, (*P).reversed, (*P).slay, (*P).chest, (*P).score_victory);
     } else {
         updateScore(fichier, P, job);
     }
-  fclose(fichier);
-  }
 
+    fclose(fichier);
+}
