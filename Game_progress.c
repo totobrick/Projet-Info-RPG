da5a6971f9432d946adf9b70a4340e10fefb5dd3
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-27 23:56:49 +0200

    Update Game_progress.c

--- Game_progress.c
+++ Game_progress.c
@@ -11,7 +11,85 @@ void resetPlayerPosition(Player *p) {
   (*p).y = (*p).y_init;
   printw("Position du joueur %s reinitialisee.\n", (*p).nom);
 }
+void updateScore(FILE* fichier, Player* P, char job[]) {
+    FILE* fichierTemp = fopen("temp.txt", "w");
+    if (fichierTemp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier temporaire.\n");
+        return;
+    }
 
+    char player_name[100];
+    char player_class[10];
+    strcpy(player_class, job);
+    int player_move;
+    int player_reversed;
+    int player_slay;
+    int player_chest;
+    int player_score_victory;
+    char ligne[100];
+  
+    rewind(fichier);
+  
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        sscanf(ligne, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, &player_move, &player_reversed, &player_slay, &player_chest, &player_score_victory);
+
+        if (strcmp(player_name, (*P).nom) == 0) {
+            player_move += (*P).move;
+            player_reversed += (*P).reversed;
+            player_slay += (*P).slay;
+            player_chest += (*P).chest;
+            player_score_victory += (*P).Score_victory;
+
+            fprintf(fichierTemp, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+        } else {
+            // Mettre à jour les valeurs des variables dans la branche else
+            player_move = player_move;
+            player_reversed = player_reversed;
+            player_slay = player_slay;
+            player_chest = player_chest;
+            player_score_victory = player_score_victory;
+            
+            fprintf(fichierTemp, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", player_name, player_class, player_move, player_reversed, player_slay, player_chest, player_score_victory);
+        }
+    }
+
+    fclose(fichier);
+    fclose(fichierTemp);
+
+    remove("Score.txt");
+    rename("temp.txt", "Score.txt");
+}
+
+void Score_creator(Player* P) {
+    FILE* fichier = fopen("Score.txt", "a+");
+
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        return;
+    }
+
+    char job[10];
+    if ((*P).classe == 1) {
+        strcpy(job, "Magicien");
+    } else if ((*P).classe == 2) {
+        strcpy(job, "Guerrier");
+    } else if ((*P).classe == 3) {
+        strcpy(job, "Ranger");
+    } else {
+        strcpy(job, "Voleur");
+    }
+
+    fseek(fichier, 0, SEEK_END);
+    long file_weight = ftell(fichier);
+
+    if (file_weight == 0) {
+        fprintf(fichier, "%s\t%s\t%d\t%d\t%d\t%d\t%d\n", (*P).nom, job, (*P).move, (*P).reversed, (*P).slay,(*P).chest, (*P).Score_victory);
+    } else {
+        updateScore(fichier, P, job);
+    }
+
+    fclose(fichier);
+}
 /*_________________________________________________________________________________________________*/
 // AVANT DE JOUER
 
