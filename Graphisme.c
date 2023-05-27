3a42a5797ac4c2d1986fb76fa1e6c2d1a7e64f79
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-27 20:28:10 +0200

    Update Graphisme.c

--- Graphisme.c
+++ Graphisme.c
@@ -1,37 +1,5 @@
-/*#include <ncurses.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {*/
-    FILE *fichier;
-    char ligne[256];
-    
-    // Ouvrir le fichier en mode lecture
-    fichier = fopen("chemin/vers/le/fichier.txt", "r");
-    if (fichier == NULL) {
-        fprintf(stderr, "Impossible d'ouvrir le fichier\n");
-        return 1;
-    }
-        while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        printw("%s", ligne);
-    }
-    
- /*   // Initialiser ncurses
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);*/
-    
-    // Lire et afficher le contenu du fichier ligne par ligne
-
-  /*  // Attendre une touche pour quitter
-    getch();
-    
-    // Terminer ncurses
-    endwin();
-    
-    // Fermer le fichier
-    fclose(fichier);
-    
-    return 0;
-}*/
+endTime = time(NULL);
+double elapsedTime = difftime(endTime, startTime);
+P.timer=P.timer + elapsedTime; 
+if (checkTreasure(P)==0 && number_of_players_who_played==number_of_players){
+    number_of_players_who_played=0;
