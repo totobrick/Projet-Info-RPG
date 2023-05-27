236ed84f47599a1826507156a9913d14e64896a9
TTtra7 <133212477+TTtra7@users.noreply.github.com>
2023-05-27 23:54:47 +0200

    Delete Graphisme.c

--- Graphisme.c
+++
@@ -1,5 +0,0 @@
-endTime = time(NULL);
-double elapsedTime = difftime(endTime, startTime);
-P.timer=P.timer + elapsedTime; 
-if (checkTreasure(P)==0 && number_of_players_who_played==number_of_players){
-    number_of_players_who_played=0;
